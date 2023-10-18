cbf903b6461d08bc825a89cd3a004b0449a99447
emaenriquez <emaenriquez1985@gmail.com>
2023-10-18 20:52:04 -0300

    nuevos cambios realizado en el proyecto hangman

--- hangman/04_hangman.py
+++ hangman/04_hangman.py
@@ -13,3 +13,18 @@ def ahorcado():
     palabra = obtener_palabra_valida(p)  # Obtiene una palabra válida de la lista "p"
     letras_de_la_palabra = set(palabra)  # Convierte la palabra en un conjunto de letras únicas
     alfabeto = set(string.ascii_uppercase)  # Crea un conjunto de letras del alfabeto en mayúsculas
+    used_letters = set()
+
+    user_letter = input('Guess a letter:  ').upper()
+    if user_letter in alfabeto - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in letras_de_la_palabra:
+            letras_de_la_palabra.remove(user_letter)
+    elif user_letter in used_letters:
+        print('you have already used that character. please try again')
+    else:
+        print('')
+
+user_input = input('Type somethig')
+
+print(user_input)
